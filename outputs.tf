540b1f0f31aa18a10592e14835041ce6b89e8844
Liam Dwyer <liam@zingeroo.com>
2022-07-11 11:52:13 -0400

    Adding new output

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,8 @@ output "aws_iam_role_arn" {
   description = "ARN of the IAM role that is being utilized by the deployed controller."
   value       = aws_iam_role.this.arn
 }
+
+output "aws_iam_role_name" {
+  description = "Name of the IAM role that is being utilized by the deployed controller."
+  value       = aws_iam_role.this.name
+}
